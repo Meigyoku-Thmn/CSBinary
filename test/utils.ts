b4123e85e46269e7d73823168b3478c44a663381
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2020-10-26 13:23:22 +0700

    disable segfault-handler

--- test/utils.ts
+++ test/utils.ts
@@ -4,8 +4,8 @@ import fs from 'fs';
 import { SeekOrigin } from '../src/constants/mode';
 import path from 'path';
 
-import SegfaultHandler from 'segfault-handler';
-SegfaultHandler.registerHandler('crash.log', () => console.log('Written to crash.log'));
+// import SegfaultHandler from 'segfault-handler';
+// SegfaultHandler.registerHandler('crash.log', () => console.log('Written to crash.log'));
 
 let File = _File;
 export const TmpFilePath = path.join(__dirname, 'tmp/f.tmp');
