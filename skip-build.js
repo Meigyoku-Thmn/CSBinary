954e9dee3be9efc89ab7e37194adc32e93d32b24
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2020-10-27 16:20:34 +0700

    so, they use powershell...

--- skip-build.js
+++ skip-build.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
-if (process.env.SKIP_BUILD)
+if (process.env.SKIP_BUILD) {
+  console.log('Skip normal build process.');
   process.exit(0);
+}
 else
   process.exit(1);
