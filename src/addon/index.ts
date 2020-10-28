0861bc9af5da2be86b9dfda4cd832391aba5615d
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2020-10-28 14:42:15 +0700

    correct native module path

--- src/addon/index.ts
+++ src/addon/index.ts
@@ -1,5 +1,8 @@
 import node_gyp_build from 'node-gyp-build';
 import path from 'path';
+let baseDir = path.join(__dirname, '../..');
+if (path.basename(baseDir) === 'dist')
+  baseDir = path.join(baseDir, '..');
 const addon = node_gyp_build(path.join(__dirname, '../..'));
 
 export const getFileState = addon.GetFileState as (fd: number) => {
